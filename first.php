104f324c6704c21520727826f6ac52d32fea258c
Bananek96 <Banasiak_Jakub@wp.pl>
2019-03-01 08:09:32 +0100

    php

---
+++ first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Test PHP</title>
+</head>
+
+<body>
+<?php
+    echo("<h1><center>Witaj w PHP!</center></h1>");
+?>
+</body>
+
+</html>
